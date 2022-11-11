9a76e3f98ad29c371de1e70c9c930f4cf129f91f
inv2004 <inv2004@gmail.com>
2022-11-12 02:36:59 +0300

    lin version

--- t.nim
+++ t.nim
@@ -13,11 +13,13 @@ when not defined windows:
   proc cmd(s: string) =
     var s = s
     s = s.multiReplace {
-            "!{F4}": "Ctrl+Q", "#": "Super_L+", "^": "Ctrl+", "!": "Alt+",
-            "`": "Grave", "{TAB}": "Tab", "{ENTER}": "Return", "{ESC}": "Esc"
-            }
+            "!{F4}": "Super_L+q", "#": "Super_L+", "^": "Ctrl+", "!": "Alt+",
+            "`": "grave", "{TAB}": "Tab", "{ENTER}": "Enter", "{F5}": "F5",
+            "{ESC}": "Escape", "{UP}": "Up", "{DOWN}": "Down",
+            "_": "underscore"
+      }
+    echo s
     let cmd = "xdotool key "&s
-    echo cmd
     if 0 != execCmd cmd:
       echo "Error during: "&cmd
       quit 1
@@ -40,7 +42,12 @@ proc alt(s: string) = key "!"&s
 proc tab() = key "{TAB}"
 proc ret() = key "{ENTER}"
 proc esc() = key "{ESC}"
-proc typ(s: string) = cmd s.split().join(" "); ret()
+proc typ(s: string) =
+  var ks: seq[string] = @[]
+  for c in s:
+    if c == ' ': ks.add "space"
+    else: ks.add $c
+  key ks.join(" "); ret()
 proc wait(ms: int) = echo "... " & $ms; sleep ms
 proc vscode() =
   win "1"
@@ -48,7 +55,7 @@ proc vscode() =
     win "d"
     typ "vscode"
   wait 10000
-  for c in 'a'..'d':
+  for c in ["as", "bi", "sy", "du"]:
     ctrl "p"
     typ $c
     ctrl "f"
@@ -60,14 +67,13 @@ proc vscode() =
     ctrl "w"
   ctrl "`"
   typ "nimble test"
-  wait 20000
+  wait 10000
   ctrl "`"
   alt "{F4}"
 
 proc browser() =
-  when defined windows:
-    win "2"
-  else:
+  win "2"
+  when not defined windows:
     win "w"
   wait 5000
   typ "youtube jess 123 video"
